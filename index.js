d39cd0c5bf7fd2210a99ba59175d335766808ced
Sujeet17m <sujeet17das@gmail.com>
2025-07-26 22:52:53 +0530

    bc

---
+++ index.js
@@ -0,0 +1,37 @@
+// let a = [1,2,3,4];
+// let b = [...a];
+// a.pop();
+// console.log(a);
+// console.log(b);
+
+// let a = {1:2,3:4};
+// let b = a;
+// a[1] = 5;
+// console.log(a);
+// console.log(b);
+
+// let a = {1:2,3:4};
+// let b = {...a};
+// a[1] = 5;
+// console.log(a);
+// console.log(b);
+
+// //for loop
+// for(let i = 0; i <= 12; i++){
+//     console.log(i);
+// }
+
+// //foreach (in array)
+// let arr = [1 , 2, 3 , 4 , 5];
+// arr.forEach(i => {
+//     console.log(i);
+    
+//     // You can use 'item' inside this block
+// });
+
+// //forin (for object)
+// let obj = {name:'harsha', age: 23};
+// for (let key in obj) {
+//     console.log(key, obj[key]);
+    
+// }
