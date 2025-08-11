eb1ac4a2b35fd73321729c93c3d84eda72883e5a
Sujeet17m <sujeet17das@gmail.com>
2025-08-11 18:52:09 +0530

    write and readfile in node.js

--- index.js
+++ index.js
@@ -37,24 +37,54 @@
 // }
 
 //callback hell
-function connectToServer(cbfn){
-    console.log("Connecting to server...");
-    setTimeout(()=>{
-        console.log("Connected to server");
-        cbfn();
-    },2000)
-}
+// function connectToServer(cbfn){
+//     console.log("Connecting to server...");
+//     setTimeout(()=>{
+//         console.log("Connected to server");
+//         cbfn();
+//     },2000)
+// }
 
-function fetchData(cbfn){
-    console.log("Fetching data...");
-    setTimeout(()=>{
-        console.log("Data fetched");
-        cbfn(["course1", "course2", "course3"]);
-    },2000);
-}
+// function fetchData(cbfn){
+//     console.log("Fetching data...");
+//     setTimeout(()=>{
+//         console.log("Data fetched");
+//         cbfn(["course1", "course2", "course3"]);
+//     },2000);
+// }
 
-connectToServer(function(){
-    fetchData(function(data){
-        console.log("Data received:", data);
-    });
+// connectToServer(function(){
+//     fetchData(function(data){
+//         console.log("Data received:", data);
+//     });
+// });
+
+// Promise example
+// function connectToServer() {        
+//     console.log("Connecting to server...");
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Connected to server");
+//             resolve();
+//         }, 2000);
+//     });
+// }
+
+//writing file using fs module
+const fs = require('fs');
+fs.writeFile("example.txt", "Hello, this is a test file.", (err) => {
+    if (err) {
+        console.error("Error writing file:", err);
+    } else {
+        console.log("File written successfully.");
+    }
+});
+
+//reading file using fs module
+fs.readFile("example.txt","Utf8",(err, data) => {
+    if (err) {
+        console.error("Error reading file:", err);
+    } else {
+        console.log("File content:", data);
+    }       
 });
