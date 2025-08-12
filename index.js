64df9ece5827b919b03dee3df35a6768cdd95f16
Sujeet17m <sujeet17das@gmail.com>
2025-08-13 00:51:44 +0530

    404

--- index.js
+++ index.js
@@ -156,12 +156,20 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = 5000;
+
 app.get('/', (req, res) => {
-    res.send("Welcome to the Express server!");
+  res.send("Welcome to the Express server!");
 }); 
+
 app.get('/about', (req, res) => {
-    res.send("This is the about page.");
+  res.send("This is the about page.");
 });
+
+// Handle 404 for undefined routes
+app.use((req, res) => {
+  res.status(404).send("404 - Page Not Found");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
